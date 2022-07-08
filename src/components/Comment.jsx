80ed2401370d7e848298b2c09e782851bf42e44a
Luiz Fernando Souza <fenna.wake@gmail.com>
2022-07-08 11:41:35 +1000

    First component: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,12 @@
+import "./Comment.css"
+
+const Comment = () => {
+    return (
+        <div className="comment">
+            <h2>Fernando Souza</h2>
+            <p>First comment component.</p>
+        </div>
+    );
+};
+
+export default Comment;
