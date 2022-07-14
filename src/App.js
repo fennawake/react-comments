c0f4cbea2dc81a76da7e189d03a9ca433495cd17
Luiz Fernando Souza <fenna.wake@gmail.com>
2022-07-14 17:09:41 +1000

    Migration from App Component to Class component and implemented React Hook State to create a new comment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
+import React from 'react';
 import './App.css';
 import Comment from './components/Comment';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My Comments App!</h1>
-      <Comment 
-        name="Fernando Souza" 
-        email="fenna.wake@gmail.com" 
-        message="First comment component with props" 
-        date={new Date()} 
-      />
-      <Comment 
-        name="John Mayer" 
-        email="johnmayer@gmail.com" 
-        message="This is other component with props" 
-        date={new Date()} 
-      />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    comments: [
+      {
+        name: "Fernando Souza",
+        email: "fenna.wake@gmail.com",
+        message: "First comment component with props, class and state",
+        date: new Date()
+      },
+      {
+        name: "John Mayer",
+        email: "johnmayer@gmail.com",
+        message: "This is other component with props, class and state",
+        date: new Date()
+      }
+    ]
+  }
+
+  addComment = () => {
+    const newComment = {
+      name: "Susan boyle",
+      email: "susan@gmail.com",
+      message: "This component was created when the button clicked!",
+      date: new Date()
+    };
+
+    this.setState({
+      comments: [...this.state.comments, newComment]
+    });
+
+    console.log("Access addComment method!");
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>My Comments App!</h1>
+        {this.state.comments.map((comment, index)=>{
+          return(
+            <Comment 
+              key= {index}
+              name= {comment.name} 
+              email= {comment.email} 
+              message= {comment.message}
+              date= {comment.date} 
+            />
+          );
+        })}
+        <button onClick={this.addComment}>Adicionar comentario</button>
+      </div>
+    );
+  }
 }
 
 export default App;
